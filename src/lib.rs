5167da126fa1a7fa6cd85d706150584fcf2677a4
Bart Massey <bart@cs.pdx.edu>
2017-11-11 18:12:02 -0800

    added Index and IndexMut support

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,18 @@
 ///!
 ///! # Examples
 ///! ```
-///! use lazy_vec::*;
+///! use lazy_vec::LazyVec;
 ///! 
 ///! let mut a: LazyVec<i8> = LazyVec::new();
-///! a.write(77, -12i8);
-///! assert_eq!(a.read(77), -12i8);
+///! a[77] = -12i8;
+///! assert_eq!(a[77], -12i8);
 ///! ```
 
 extern crate alloc;
 
 use std::ptr;
 use alloc::raw_vec::RawVec;
+use std::ops::{Index, IndexMut};
 
 ///! This opaque structure stores a lazy vector.
 pub struct LazyVec<T> {
@@ -71,11 +72,11 @@ impl <T: Copy> LazyVec<T> {
         self.size
     }
 
-    ///! Read the value at index `i`.
+    ///! Return a reference to the value at the given index.
     ///!
     ///! XXX For now, panic on attempt to read from an
     ///! uninitialized element.
-    pub fn read(&self, i: usize) -> T {
+    pub fn value_ref(&self, i: usize) -> &T {
         // XXX For now, fail if the value has not been
         // initialized (index off end of indices).
         assert!(i < self.size);
@@ -94,11 +95,13 @@ impl <T: Copy> LazyVec<T> {
         // value).
         assert!(self.value_indices[ix] == i);
         // Return the correct value from the value stack.
-        self.values[ix]
+        &self.values[ix]
     }
 
-    ///! Write the value `v` at index `i`.
-    pub fn write(&mut self, i: usize, v: T) {
+    ///! Return a mutable reference to the value at index `i`.
+    ///! If no value previously existed, this will return a reference
+    ///! to uninitialized memory, making it unsafe.
+    pub unsafe fn value_ref_mut(&mut self, i: usize) -> &mut T {
         // Get the current index capacity.
         let cap = self.indices.cap();
         // If the current index capacity is too small, grow it.
@@ -106,28 +109,45 @@ impl <T: Copy> LazyVec<T> {
             self.indices.reserve(cap, i - cap);
         }
         // Get a pointer to the index element.
-        let ixptr = unsafe{ self.indices.ptr().offset(i as isize) };
+        let ixptr = self.indices.ptr().offset(i as isize);
         // Get the current index.
-        let ix = unsafe{ ptr::read(ixptr) };
+        let ix = ptr::read(ixptr);
         // Get the stack top.
         let nstacked = self.values.len();
         assert!(nstacked == self.value_indices.len());
         // If the value is uninitialized, initialize it.
         // Otherwise, just store it.
         if ix >= nstacked || self.value_indices[ix] != i {
-            // Save the value on the stack.
-            self.values.push(v);
+            // Save a place for a value on the stack.
+            self.values.reserve(1);
+            self.values.set_len(nstacked + 1);
             // Save the index of the value on the stack.
             self.value_indices.push(i);
             // Save the index of the value to the index.
-            unsafe{ ptr::write(ixptr, nstacked) };
+            ptr::write(ixptr, nstacked);
             // Increase the size if necessary.
             if ix >= self.size {
                 self.size = i + 1
-            }
-        } else {
-            self.values[ix] = v
-        }
+            };
+            &mut self.values[nstacked];
+        };
+        &mut self.values[ix]
+    }
+}
+
+impl<T: Copy> Index<usize> for LazyVec<T> {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, i: usize) -> &T {
+        self.value_ref(i)
+    }
+}
+
+impl<T: Copy> IndexMut<usize> for LazyVec<T> {
+    #[inline]
+    fn index_mut(&mut self, i: usize) -> &mut T {
+        unsafe{ self.value_ref_mut(i) }
     }
 }
 
@@ -135,14 +155,14 @@ impl <T: Copy> LazyVec<T> {
 #[should_panic]
 fn test_miss_off_end() {
     let mut a: LazyVec<i8> = LazyVec::new();
-    a.write(77, -12i8);
-    let _ = a.read(78);
+    a[77] = -12i8;
+    let _ = a[100000];
 }
 
 #[test]
 #[should_panic]
 fn test_miss_uninit() {
     let mut a: LazyVec<i8> = LazyVec::new();
-    a.write(77, -12i8);
-    let _ = a.read(76);
+    a[77] = -12i8;
+    let _ = a[76];
 }
